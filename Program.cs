794fb3ae273ab02d3efb274718e6a94a2e7e056a
polvonovsadriddin <144240322+polvonovsadriddin@users.noreply.github.com>
2023-11-19 22:39:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//вариант 5 высокий     
+Console.Write("Введите размер массива:");
+int n = int.Parse(Console.ReadLine());
+int [] mas = new int [n];
+Random rnd = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = rnd.Next(10,100) ;
+    Console.Write(Convert.ToString(mas[i], 2) + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < mas.Length; i++)
+{
+    
+    Console.Write(Convert.ToString(mas[i], 8) + " ");
+}
